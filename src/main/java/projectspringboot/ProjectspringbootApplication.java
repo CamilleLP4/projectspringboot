b850f612183b7863b0170305819691f0b1f30d61
moi <camille.paudrat.lp4@gmail.com>
2019-12-11 17:35:16 +0100

    first commit

---
+++ src/main/java/projectspringboot/ProjectspringbootApplication.java
@@ -0,0 +1,13 @@
+package projectspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projectspringboot/ProjectspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package projectspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
